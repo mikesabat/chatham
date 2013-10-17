b2f6b7b422155e9d487527fee255338e6e8819c2
Michael <mike.sabat@gmail.com>
2013-10-16 21:20:07 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatham::Application.config.secret_token = '13be49dc1734fa93269b7625031b4d82a07b64b8e48256c0b5466a8b1dcc220a8d974837c49b7072121650e903de52dc523d7eafc7ca7e286def2aad3d4d8e2e'
